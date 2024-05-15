71ce6c947e7755fce0d762b49c3aef63acd3a46f
Anh Le <itoutsource27@generali-life.com.vn>
2024-05-15 14:55:15 +0700

    updated

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const startBtn = document.getElementById("startBtn");
 const stopBtn = document.getElementById("stopBtn");
 const uploadBtn = document.getElementById("uploadBtn");
-const statusCircle = document.getElementById("statusCircle");
 const recordingLengthText = document.getElementById("recordingLength");
-const recordedTimeLabel = document.getElementById("recordedTimeLabel");
 let mediaRecorder;
 let recordedChunks = [];
 let recordingStartTime;
@@ -14,25 +12,15 @@ let stream; // Define stream variable in a broader scope
 stopBtn.disabled = true;
 
 document.addEventListener("DOMContentLoaded", () => {
-  const permission = localStorage.getItem("audioPermission");
-  if (permission === "granted") {
-    console.log(
-      "Audio recording permission already granted. Proceed with recording."
-    );
-    stream = navigator.mediaDevices.getUserMedia();
-    // Code to start audio recording
-  } else {
-    navigator.mediaDevices
-      .getUserMedia({ audio: true })
-      .then((userStream) => {
-        localStorage.setItem("audioPermission", "granted");
-        stream = userStream;
-        startBtn.disabled = false; // Enable start button once permission is granted
-      })
-      .catch((error) => {
-        console.error("Error accessing microphone:", error);
-      });
-  }
+  navigator.mediaDevices
+    .getUserMedia({ audio: true })
+    .then((userStream) => {
+      stream = userStream;
+      startBtn.disabled = false; // Enable start button once permission is granted
+    })
+    .catch((error) => {
+      console.error("Error accessing microphone:", error);
+    });
 });
 
 startBtn.addEventListener("click", () => {
@@ -46,7 +34,6 @@ startBtn.addEventListener("click", () => {
     mediaRecorder.start();
 
     // Update status indicator to show recording status
-    statusCircle.style.backgroundColor = "red";
     recordingStartTime = Date.now();
 
     // Start interval to update recording length text and recorded time label
@@ -57,12 +44,12 @@ startBtn.addEventListener("click", () => {
         .padStart(2, "0");
       const seconds = (elapsedTime % 60).toString().padStart(2, "0");
       recordingLengthText.textContent = `Recording Length: ${minutes}:${seconds}`;
-      recordedTimeLabel.textContent = `Recorded Time: ${minutes}:${seconds}`;
     }, 1000); // Update every second
 
     // Enable the stop button and disable the start button
     stopBtn.disabled = false;
     startBtn.disabled = true;
+    uploadBtn.disabled = true;
   }
 });
 
@@ -87,21 +74,36 @@ stopBtn.addEventListener("click", () => {
       // Enable the start button and disable the stop button
       stopBtn.disabled = true;
       startBtn.disabled = false;
+      uploadBtn.disabled = false;
 
       // Update status indicator to show idle status
-      statusCircle.style.backgroundColor = "grey"; // Change color to grey
       recordingLengthText.textContent = ""; // Reset recording length text
 
       // Clear recording length interval
       clearInterval(recordingLengthInterval); // Stop updating the recording length text
-
-      // Reset the recorded time label
-      recordedTimeLabel.textContent = "";
     };
   }
 });
 
 uploadBtn.addEventListener("click", () => {
-  // Implement file upload logic to your API here
-  // You can use fetch API or XMLHttpRequest to send the recorded audio file to the server
+  uploadBtn.disabled = true;
+  // Upload the audio file
+  const audioBlob = new Blob(recordedChunks, { type: "audio/wav" });
+  // Assuming formData contains the audio file data
+  const formData = new FormData();
+  formData.append("audio", audioBlob, "audio.wav");
+  fetch("http://localhost:3000/upload_audio", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      uploadBtn.disabled = false;
+      alert("Audio file uploaded successfully");
+    })
+    .catch((error) => {
+      uploadBtn.disabled = false;
+      alert("Failed!");
+    });
 });
